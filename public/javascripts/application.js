72f37bbd5c440417c6d14e7fa88e3280bcb60807
Shoaib Burq <saburq@gmail.com>
2008-08-09 16:25:38 +1000

    added images for front page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+/*
+Highlight Link script
+By Website Abstraction (http://wsabstract.com)
+and Java-scripts.net (http://www.java-scripts.net)
+*/
+
+function highlight(which,color){
+	if (document.all||document.getElementById)
+	which.style.backgroundColor=color
+}
