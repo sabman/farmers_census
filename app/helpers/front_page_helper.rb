201bea56edd371b23bf3d619eab052552b192051
Shoaib Burq <saburq@gmail.com>
2008-08-09 13:50:09 +1000

    limited the panning

--- app/helpers/front_page_helper.rb
+++ app/helpers/front_page_helper.rb
@@ -5,9 +5,10 @@ module FrontPageHelper
   
   def show_maps recs
     @map = GMap.new("map_div", "map")
-    @map.control_init(:small_map => true, :hierarchical_map_type => true)
+    @map.control_init(:small_map => true, :map_type => false)
     @map.record_init @map.add_map_type(GMapType::G_PHYSICAL_MAP)    
     @map.set_map_type_init(GMapType::G_PHYSICAL_MAP)
+
     coords = [] 
     recs.each do |rec|
       unless rec.lng == nil or rec.lat == nil
@@ -23,62 +24,94 @@ module FrontPageHelper
    # @map.center_zoom_on_bounds_init([[bnd.lower_corner.y,bnd.lower_corner.x],[bnd.upper_corner.y,bnd.upper_corner.x]])
     @map.center_zoom_init([39.5,-97],4)   
     
-    @map.record_init '    
-    // Rectangle Overlay
+    @map.record_init( '      
+
+ 
+    // remove copyright
+    map.removeMapType(G_NORMAL_MAP);
+    map.removeMapType(G_SATELLITE_MAP);
+    var myCopyright = new GCopyrightCollection("© ");
+    myCopyright.addCopyright(new GCopyright(\'Greenhorns\',
+      new GLatLngBounds(new GLatLng(-90,-180), new GLatLng(90,180)), 
+      0,\'©2008 Greenhorns\'));
+    
+    // Rectangle Overlay - init and show
     var rectBounds = new GLatLngBounds(new GLatLng(-40, -179), new GLatLng(80,-1));
     var rectOverlay = new Rectangle(rectBounds);
     map.addOverlay(rectOverlay); 
-    rectOverlay.hide();   
+    rectOverlay.show();   
     
     // create the bottom left/top right coord for the overlay and add it as a 
   	// GGroundOverlay pinning it to those bbox coords
-  	// MAP_DETAILED.JPG
+  	// MAP_DETAILED.JPG - init and hide
   	var boundaries1 = new GLatLngBounds(
   			new GLatLng(22.727,-127.594), new GLatLng(56.417,-64.563));	
   	var mapDetailedOverlay = new GGroundOverlay("/images/handdrawn/map_detailed.jpg", boundaries1);  																					
     mapDetailedOverlay.hide();																					
   	map.addOverlay(mapDetailedOverlay); 
 
-  	// MAP_SMALL.JPG
+  	// MAP_SMALL.JPG - init and hide
   	var boundaries2 = new GLatLngBounds(
   			new GLatLng(20.64,-127.57), new GLatLng(54.48,-64.42));	
   	var mapSmallOverlay = new GGroundOverlay("/images/handdrawn/map_small.jpg", boundaries2);  																					
     //mapDetailedOverlay.hide();																					
   	map.addOverlay(mapSmallOverlay); 
-               
-  	// MAP_LARGE.JPG
-  	var boundaries3 = new GLatLngBounds(
-  	 		new GLatLng(21.166,-125.606), new GLatLng(53.181,-62.484));	  	 
-    var mapLargeOverlay = new GGroundOverlay("/images/handdrawn/map_large.jpg", boundaries3);  
-    mapLargeOverlay.hide();																					
- 	  map.addOverlay(mapLargeOverlay);
- 	     
+                	     
   	GEvent.addListener(map, "zoomend", function(oldz, newz){
-  		if(newz < 3) { // 0-3-4 show only mapLargeOverlay  
-  			if(rectOverlay.isHidden()){rectOverlay.show();}
-  			if(mapLargeOverlay.isHidden()){mapLargeOverlay.show();}
-  	    if(!mapSmallOverlay.isHidden()){mapSmallOverlay.hide();}
-        if(!mapDetailedOverlay.isHidden()){ mapDetailedOverlay.hide();}   
-  		} 
-  		else if(newz < 5) { // 3-4 show only mapSmallOverlay and rectOverlay 
-  			if(!mapLargeOverlay.isHidden()){mapLargeOverlay.hide();}
+      if(newz < 6) { // 0-4 show only mapSmallOverlay and rectOverlay 
   			if(!mapDetailedOverlay.isHidden()){mapDetailedOverlay.hide();}
     	  if(rectOverlay.isHidden()){rectOverlay.show();}
   			if(mapSmallOverlay.isHidden()){mapSmallOverlay.show();}
   		} 
-  		else if(newz >= 5 && newz <= 7) { // 5-7 only show mapDetailedOverlay  
-  			if(!rectOverlay.isHidden()){rectOverlay.hide();}
+  		else if(newz >= 6 && newz <= 7) { // 6-7 only show mapDetailedOverlay and recOverlay 
+  			if(rectOverlay.isHidden()){rectOverlay.show();}
    			if(mapDetailedOverlay.isHidden()){ mapDetailedOverlay.show();}
-   			if(!mapLargeOverlay.isHidden()){mapLargeOverlay.hide();}
     	  if(!mapSmallOverlay.isHidden()){mapSmallOverlay.hide();}   			  
   		} 
   		else { // for everything else no overlays
   			if(!rectOverlay.isHidden()){rectOverlay.hide();}
    			if(!mapDetailedOverlay.isHidden()){ mapDetailedOverlay.hide();}
-  			if(!mapLargeOverlay.isHidden()){mapLargeOverlay.hide();}
     	  if(!mapSmallOverlay.isHidden()){mapSmallOverlay.hide();}
-  		}}); '
-  	
+  		}}); 
+
+      // Overwrite the getMinimumResolution() and getMaximumResolution() methods
+      var mt = map.getMapTypes();
+      for (var i=0; i<mt.length; i++) {
+        mt[i].getMinimumResolution = function() {return 3;}
+      }  		
+      // Add a move listener to restrict the bounds range
+      GEvent.addListener(map, "move", function() {
+        checkBounds();
+      });
+
+      // The allowed region which the whole map must be within
+      var allowedBounds = new GLatLngBounds(new GLatLng(13.75,-140), new GLatLng(56.9,-55));
+      
+      // If the map position is out of range, move it back
+      function checkBounds() {
+        // Perform the check and return if OK
+        if (allowedBounds.contains(map.getCenter())) {
+          return;
+        }
+        // It`s not OK, so find the nearest allowed point and move there
+        var C = map.getCenter();
+        var X = C.lng();
+        var Y = C.lat();
+
+        var AmaxX = allowedBounds.getNorthEast().lng();
+        var AmaxY = allowedBounds.getNorthEast().lat();
+        var AminX = allowedBounds.getSouthWest().lng();
+        var AminY = allowedBounds.getSouthWest().lat();
+
+        if (X < AminX) {X = AminX;}
+        if (X > AmaxX) {X = AmaxX;}
+        if (Y < AminY) {Y = AminY;}
+        if (Y > AmaxY) {Y = AmaxY;}
+        //alert ("Restricting "+Y+" "+X);
+        map.setCenter(new GLatLng(Y,X));
+      }      
+  		')  	
+  		
     @map.to_html 
   end
 end
--- public/javascripts/mapping_helpers.js
+++ public/javascripts/mapping_helpers.js
@@ -1,49 +1,49 @@
 function create_draggable_marker_for_edit(lng, lat) { 
-// initalize form fields 
-document.getElementById('lng').value = lng; 
-document.getElementById('lat').value = lat; 
-// initalize marker 
-var currMarker = new GMarker( new GLatLng(lat, lng), {draggable: true} ); 
-map.addOverlay(currMarker); 
-// Handle drag events to update the form text fields 
-GEvent.addListener(currMarker, 'drag', function() { 
-document.getElementById('lng').value = currMarker.getPoint().lng(); 
-document.getElementById('lat').value = currMarker.getPoint().lat(); 
-}); 
+	// initalize form fields 
+	document.getElementById('lng').value = lng; 
+	document.getElementById('lat').value = lat; 
+	// initalize marker 
+	var currMarker = new GMarker( new GLatLng(lat, lng), {draggable: true} ); 
+	map.addOverlay(currMarker); 
+	// Handle drag events to update the form text fields 
+	GEvent.addListener(currMarker, 'drag', function() { 
+		document.getElementById('lng').value = currMarker.getPoint().lng(); 
+		document.getElementById('lat').value = currMarker.getPoint().lat(); 
+	}); 
 } 
 
 
 function create_draggable_editable_marker() 
 { 
-// intialize the values in form fields to 0 
-document.getElementById("lng").value = 0; 
-document.getElementById("lat").value = 0; 
-var currMarker; 
-// if the user click on an existing marker remove it 
-GEvent.addListener(map, "click", function(marker, point) { 
-if (marker) { 
-if (confirm("Do you want to delete marker?")) { 
-map.removeOverlay(marker); 
-} 
-} 
-// if the user clicks somewhere other than existing marker 
-else { 
-// remove the previous marker if it exists 
-if (currMarker) { 
-map.removeOverlay(currMarker); 
-} 
-currMarker = new GMarker(point, {draggable: true}); 
-map.addOverlay(currMarker); 
-// update the form fields 
-document.getElementById("lng").value = point.x; 
-document.getElementById("lat").value = point.y; 
-} 
-// Similarly drag event is used to update the form fields 
-GEvent.addListener(currMarker, "drag", function() { 
-document.getElementById("lng").value = currMarker.getPoint().lng(); 
-document.getElementById("lat").value = currMarker.getPoint().lat(); 
-}); 
-}); 
+	// intialize the values in form fields to 0 
+	document.getElementById("lng").value = 0; 
+	document.getElementById("lat").value = 0; 
+	var currMarker; 
+	// if the user click on an existing marker remove it 
+	GEvent.addListener(map, "click", function(marker, point) { 
+		if (marker) { 
+			if (confirm("Do you want to delete marker?")) { 
+				map.removeOverlay(marker); 
+			} 
+		} 
+		// if the user clicks somewhere other than existing marker 
+		else { 
+			// remove the previous marker if it exists 
+			if (currMarker) { 
+				map.removeOverlay(currMarker); 
+			} 
+			currMarker = new GMarker(point, {draggable: true}); 
+			map.addOverlay(currMarker); 
+			// update the form fields 
+			document.getElementById("lng").value = point.x; 
+			document.getElementById("lat").value = point.y; 
+		} 
+	// Similarly drag event is used to update the form fields 
+		GEvent.addListener(currMarker, "drag", function() { 
+			document.getElementById("lng").value = currMarker.getPoint().lng(); 
+			document.getElementById("lat").value = currMarker.getPoint().lat(); 
+		}); 
+	});    
 } 
 
 
@@ -56,8 +56,7 @@ function Rectangle(bounds, opt_weight, opt_color) {
   this.color_ = "#FFFFFF"; //opt_color || "#888888"; 
 	this.opacity_ = 0.5;
 	this.backgroundColor_ = "#FFFFFF";
-	this.color_ = "#FFFFFF";
-	
+	this.color_ = "#FFFFFF";	
 }
 Rectangle.prototype = new GOverlay();
 
