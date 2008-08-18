7f511414f25c46508d8dca044de5ff921fe91dfc
Shoaib Burq <saburq@gmail.com>
2008-08-19 00:09:22 +1000

    redcloth fix

--- config/environment.rb
+++ config/environment.rb
@@ -64,7 +64,7 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
-  config.gem "RedCloth", :version => ">= 4.0.1", :source => "http://code.whytheluckystiff.net/" 
+  config.gem "RedCloth", :version => ">= 4.0.1", :lib => "redcloth",  :source => "http://code.whytheluckystiff.net/" 
   config.gem "chronic", :version => ">= 0.2.3" 
   config.gem "haml", :version => ">= 2.0.2" 
 end
