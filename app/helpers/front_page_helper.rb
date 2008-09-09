d2c72dee3c66849046afbd8b49ff2d5a28ca2a9f
Mikel Maron <mikel@cormorant.(none)>
2008-09-08 21:50:58 -0700

    map tiles

--- app/helpers/front_page_helper.rb
+++ app/helpers/front_page_helper.rb
@@ -23,115 +23,107 @@ module FrontPageHelper
     coords = (coords.length == 0) ? [[-121.640625,26.431228],[-68.554687,47.15984]] : coords # if there are no records just zoom/center over 0,0      
    # bnd = MultiPoint.from_coordinates(coords).envelope
    # @map.center_zoom_on_bounds_init([[bnd.lower_corner.y,bnd.lower_corner.x],[bnd.upper_corner.y,bnd.upper_corner.x]])
-    @map.center_zoom_init([34.5,-97.6],4)   
+    @map.center_zoom_init([37.5,-93.6],4)   
     
     @map.record_init( '      
+				var copyright = new GCopyright(1, new GLatLngBounds(new GLatLng(-90,-180), new GLatLng(90,180)), 0, "greenhorns");
+				var copyrightCollection = new GCopyrightCollection("greenhorns");
+				copyrightCollection.addCopyright(copyright);
 
- 
-    // remove copyright
-    map.removeMapType(G_NORMAL_MAP);
-    map.removeMapType(G_SATELLITE_MAP);
-    var myCopyright = new GCopyrightCollection("© ");
-    myCopyright.addCopyright(new GCopyright(\'Greenhorns\',
-      new GLatLngBounds(new GLatLng(-90,-180), new GLatLng(90,180)), 
-      0,\'©2008 Greenhorns\'));
-    
-    // Rectangle Overlay - init and show
-    var rectBounds = new GLatLngBounds(new GLatLng(-40, -179), new GLatLng(80,-1));
-    var rectOverlay = new Rectangle(rectBounds);
-    map.addOverlay(rectOverlay); 
-    rectOverlay.show();   
-    
-    // create the bottom left/top right coord for the overlay and add it as a 
-  	// GGroundOverlay pinning it to those bbox coords
-  	// MAP_DETAILED.JPG - init and hide
-  	var boundaries1 = new GLatLngBounds(
-  			new GLatLng(22.727,-127.594), new GLatLng(56.417,-64.563));	
-  	var mapDetailedOverlay = new GGroundOverlay("/images/handdrawn/map_detailed.jpg", boundaries1);  																					
-    mapDetailedOverlay.hide();																					
-  	map.addOverlay(mapDetailedOverlay); 
-/*
-var copyright = new GCopyright(1, new GLatLngBounds(new GLatLng(-90,-180), new GLatLng(90,180)), 0, "greenhorns");
-var copyrightCollection = new GCopyrightCollection("greenhorns");
-copyrightCollection.addCopyright(copyright);
+				var tilelayers = [];
+				tilelayers[0] = new GTileLayer(copyrightCollection, 1, 18);
 
-var tilelayers = [];
-tilelayers[0] = new GTileLayer(copyrightCollection, 1, 3);
-tilelayers[0].getTileUrl = function (a, b) {
-return "/images/tiles/"+b+"/"+a.x+"/"+a.y+".png";
-};
-tilelayers[0].isPng = function() { return true;};
-tilelayers[0].getOpacity = function() { return 1.0; };
+				function TileToQuadKey (x, y, zoom){
+    			var quad = "";
+    			for (var i = zoom; i > 0; i--){
+        		var mask = 1 << (i - 1);
+        		var cell = 0;
+        		if ((x & mask) != 0)
+            	cell++;
+        		if ((y & mask) != 0)
+            	cell += 2;
+        		quad += cell;
+    			}
+    			return quad;
+				}
 
-var custommap = new GMapType(tilelayers, new GMercatorProjection(19), "greenhorns", {errorMessage:"Greenhorns"});
-map.addMapType(custommap);
-map.setMapType(custommap);*/
+				tilelayers[0].getTileUrl = function (a, b) {
+					return "/images/tiles/" + TileToQuadKey(a.x,a.y,b) + ".png"; 
+				};
+				tilelayers[0].isPng = function() { return true;};
+				tilelayers[0].getOpacity = function() { return 1.0; };
 
+				var custommap = new GMapType(tilelayers, new GMercatorProjection(19), "greenhorns", {errorMessage:"Greenhorns"});
+				map.addMapType(custommap);
+				map.setMapType(custommap);
+               	     
+ 			 	GEvent.addListener(map, "zoomend", function(oldz, newz){
+    	 		if(newz > 6) {
+						map.setMapType(G_PHYSICAL_MAP);
+  				} else {
+						map.setMapType(custommap);
+					}
+				}); 
 
-  	// MAP_SMALL.JPG - init and hide
-  	var boundaries2 = new GLatLngBounds(
-  			new GLatLng(20.64,-127.57), new GLatLng(54.48,-64.42));	
-  	var mapSmallOverlay = new GGroundOverlay("/images/handdrawn/map_small.jpg", boundaries2);  																					
-    //mapDetailedOverlay.hide();																					
-  	map.addOverlay(mapSmallOverlay); 
-                	     
-  	GEvent.addListener(map, "zoomend", function(oldz, newz){
-      if(newz < 6) { // 0-4 show only mapSmallOverlay and rectOverlay 
-        remove_copyright(); 
-  			if(!mapDetailedOverlay.isHidden()){mapDetailedOverlay.hide();}
-    	  if(rectOverlay.isHidden()){rectOverlay.show();}
-//  			if(mapSmallOverlay.isHidden()){mapSmallOverlay.show();}
-  		} 
-  		else if(newz >= 6 && newz <= 7) { // 6-7 only show mapDetailedOverlay and recOverlay 
-  		  remove_copyright();    
-  			if(rectOverlay.isHidden()){rectOverlay.show();}
-   			if(mapDetailedOverlay.isHidden()){ mapDetailedOverlay.show();}
-//    	  if(!mapSmallOverlay.isHidden()){mapSmallOverlay.hide();}   			  
-  		} 
-  		else { // for everything else no overlays
-  		  show_copyright();
-  			if(!rectOverlay.isHidden()){rectOverlay.hide();}
-   			if(!mapDetailedOverlay.isHidden()){ mapDetailedOverlay.hide();}
-//    	  if(!mapSmallOverlay.isHidden()){mapSmallOverlay.hide();}
-  		}}); 
+ 	     	// Overwrite the getMinimumResolution() and getMaximumResolution() methods
+      	var mt = map.getMapTypes();
+      	for (var i=0; i<mt.length; i++) {
+        	mt[i].getMinimumResolution = function() {return 3;}
+      	}	
+ 	      // Add a move listener to restrict the bounds range
+   		  GEvent.addListener(map, "move", function() {
+        	checkBounds();        
+      	});
 
-      // Overwrite the getMinimumResolution() and getMaximumResolution() methods
-      var mt = map.getMapTypes();
-      for (var i=0; i<mt.length; i++) {
-        mt[i].getMinimumResolution = function() {return 3;}
-      }  		
-      // Add a move listener to restrict the bounds range
-      GEvent.addListener(map, "move", function() {
-        checkBounds();        
-      });
-
-      // The allowed region which the whole map must be within
-      var allowedBounds = new GLatLngBounds(new GLatLng(13.75,-140), new GLatLng(56.9,-55));
+      	// The allowed region which the whole map must be within
+      	var allowedBounds = new GLatLngBounds(new GLatLng(13.75,-140), new GLatLng(56.9,-55));
       
-      // If the map position is out of range, move it back
-      function checkBounds() {
-        // Perform the check and return if OK
-        if (allowedBounds.contains(map.getCenter())) {
-          return;
-        }
-        // It`s not OK, so find the nearest allowed point and move there
-        var C = map.getCenter();
-        var X = C.lng();
-        var Y = C.lat();
+      	// If the map position is out of range, move it back
+      	function checkBounds() {
+        	// Perform the check and return if OK
+        	if (allowedBounds.contains(map.getCenter())) {
+          	return;
+        	}
+        	// It`s not OK, so find the nearest allowed point and move there
+       	 	var C = map.getCenter();
+        	var X = C.lng();
+        	var Y = C.lat();
 
-        var AmaxX = allowedBounds.getNorthEast().lng();
-        var AmaxY = allowedBounds.getNorthEast().lat();
-        var AminX = allowedBounds.getSouthWest().lng();
-        var AminY = allowedBounds.getSouthWest().lat();
+        	var AmaxX = allowedBounds.getNorthEast().lng();
+        	var AmaxY = allowedBounds.getNorthEast().lat();
+        	var AminX = allowedBounds.getSouthWest().lng();
+       	 	var AminY = allowedBounds.getSouthWest().lat();
 
-        if (X < AminX) {X = AminX;}
-        if (X > AmaxX) {X = AmaxX;}
-        if (Y < AminY) {Y = AminY;}
-        if (Y > AmaxY) {Y = AmaxY;}
-        //alert ("Restricting "+Y+" "+X);
-        map.setCenter(new GLatLng(Y,X));
-      }     
-  		remove_copyright();
+        	if (X < AminX) {X = AminX;}
+        	if (X > AmaxX) {X = AmaxX;}
+        	if (Y < AminY) {Y = AminY;}
+        	if (Y > AmaxY) {Y = AmaxY;}
+        	//alert ("Restricting "+Y+" "+X);
+        	map.setCenter(new GLatLng(Y,X));
+      	}
+
+				function getElementsByClass(searchClass,node,tag) {
+					var classElements = new Array();
+					if ( node == null )
+					node = document;
+					if ( tag == null )
+						tag = "*";
+					var els = node.getElementsByTagName(tag);
+					var elsLen = els.length;
+					var pattern = new RegExp("(^|\\\\s)"+searchClass+"(\\\\s|$)");
+					for (i = 0, j = 0; i < elsLen; i++) {
+						if ( pattern.test(els[i].className) ) {
+							classElements[j] = els[i];
+							j++;
+						}
+					}
+					return classElements;
+				}
+				var gmnoprint = getElementsByClass("gmnoprint");
+				for (i = 0; i < gmnoprint.length; i++) {
+					gmnoprint[i].style.visibility = "hidden";
+				} 
+    
   		')  	
   		
     @map.to_html 
