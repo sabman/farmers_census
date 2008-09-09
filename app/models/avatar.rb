619a4518561931d0698918c785a2fa154a068004
Shoaib Burq <saburq@gmail.com>
2008-09-10 06:32:01 +1000

    added image_science to do the processing

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -2,6 +2,7 @@ class Avatar < ActiveRecord::Base
   has_attachment :content_type => :image, 
                  :storage => :file_system, 
                  :max_size => 3.megabytes, 
+                 :processor => :image_science,
                  #:resize_to => '110x110>',
                  :thumbnails => {:thumb => "110x110>"}
   
