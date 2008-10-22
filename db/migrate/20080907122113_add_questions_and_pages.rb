9ddab01e798ec8be92e129034c44233864194e0e
Shoaib Burq <saburq@gmail.com>
2008-10-22 11:40:05 +1100

    minor

--- db/migrate/20080907122113_add_questions_and_pages.rb
+++ db/migrate/20080907122113_add_questions_and_pages.rb
@@ -3,7 +3,7 @@ class AddQuestionsAndPages < ActiveRecord::Migration
     # exec('rake db:questions:load; rake db:pages:load')
     # should invokee this manually. On the server run the following:
     # cap invoke COMMAND="cd /data/rails/current; rake RAILS_ENV=express  db:questions:load"  HOSTS="nomad-berlin.dyndns.org"
-    # cap invoke COMMAND="cd /data/rails/current; rake RAILS_ENV=express  db:pages:load" HOSTS="nomad-berlin.dyndns.org"
+    # cap invoke COMMAND="cd /data/rails/current; rake RAILS_ENV=express  db:pages:load" HOSTS="www.serveyourcountryfood.net"
   end
 
   def self.down
