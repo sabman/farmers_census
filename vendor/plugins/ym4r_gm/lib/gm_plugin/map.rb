ce3cef30488568e506603c392ed2aec960793b7d
Shoaib Burq <saburq@gmail.com>
2008-10-26 15:55:21 +1100

    hack to fix the google maps breaking with the new version when used with scriptaculous http://code.google.com/p/gmaps-api-issues/issues/detail?id=795 - :(

--- vendor/plugins/ym4r_gm/lib/gm_plugin/map.rb
+++ vendor/plugins/ym4r_gm/lib/gm_plugin/map.rb
@@ -31,6 +31,7 @@ module Ym4r
         options[:hl] ||= ''
         options[:local_search] = false unless options.has_key?(:local_search)
         api_key = ApiKey.get(options) 
+
         a = "<script src=\"http://maps.google.com/maps?file=api&amp;v=2.132d&amp;key=#{api_key}&amp;hl=#{options[:hl]}\" type=\"text/javascript\"></script>\n"
         a << "<script src=\"/javascripts/ym4r-gm.js\" type=\"text/javascript\"></script>\n" unless options[:without_js]
         a << "<style type=\"text/css\">\n v\:* { behavior:url(#default#VML);}\n</style>" if options[:with_vml]
